bd3890cb9d100dd55b0cee665cb197bd749d56dd
nkmbxy <nakamon22547@gmail.com>
2024-06-08 04:27:24 +0700

    edit user route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,6 +5,6 @@ const userController = require("../controllers/user.controller");
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
-router.post("/userByUserID", userController.userByUserID);
+router.get("/userByUserID", userController.userByUserID);
 
 module.exports = router;
