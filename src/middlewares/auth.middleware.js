70a9ce8e51f9889a5ea512962b186aa51e7cc642
nkmbxy <nakamon22547@gmail.com>
2024-06-07 21:52:45 +0700

    edit verifyToken

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,28 +2,14 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   try {
-    const bearerHeader = req?.headers["authorization"];
-    if (!bearerHeader) {
-      return res.json({
-        data: "a token is required for authentication",
-        status: 401,
-      });
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
-    const bearer = bearerHeader.split(" ");
-    if (bearer.length !== 2 || bearer[0] !== "Bearer") {
-      return res.status(401).json({
-        data: "Malformed token",
-        status: 401,
-      });
-    }
-    const bearerToken = bearer[1];
-    const tokenDecoded = jwt.verify(bearerToken, "HotTwoHot");
-    req.user = tokenDecoded;
+    next();
   } catch (error) {
-    console.log(error);
-    return res.json({ data: "invalid token", status: 401 });
+    console.log(error.message);
   }
-  return next();
 };
 
 module.exports = verifyToken;
