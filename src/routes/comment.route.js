605e3d9e4996ef08e3b138acf0c96ac3e2ec27be
nkmbxy <nakamon22547@gmail.com>
2024-06-09 20:58:55 +0700

    edit comment and review

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/comment.controller");
-const verifyToken = require("../middlewares/auth.middleware");
 
 router.post("/createComment", commentController.createComment);
 router.post("/like/:commentId", commentController.likeComment);
